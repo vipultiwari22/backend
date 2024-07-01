d903812c636bd9ad1280be16b9fc34f871b01ab0
vipultiwari22 <tiwarivipul155@gmail.com>
2024-07-01 13:41:53 +0530

    commenting

--- URLShortnerProject/app.js
+++ URLShortnerProject/app.js
@@ -1,3 +1,5 @@
+// Import all importent file and Moudules
+
 import dotenv from "dotenv";
 import path from "path";
 import express from "express";
@@ -6,21 +8,35 @@ import urlRouter from "./routes/url.route.js"; // Ensure the correct path
 import URL from "./model/url.model.js";
 import StaticRoute from "./routes/static.route.js";
 
+// use Dotenv and execute
+
 dotenv.config();
+
+// PORT and express instance
 
 const app = express();
 const PORT = 8001;
 
+// connection Establish with DB
+
 connectDB();
+
+// setup EJS
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// use json and form data Parser
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Urls for the APIs
+
 app.use("/url", urlRouter);
 app.use("/", StaticRoute);
+
+// redirect to the url Function
 
 app.get("/urls/:shortId", async (req, res) => {
   const shortID = req.params.shortId;
@@ -39,6 +55,7 @@ app.get("/urls/:shortId", async (req, res) => {
   res.redirect(entry.redirectURL);
 });
 
+// listing to the port to run server
 app.listen(PORT, () => {
   console.log(`Server Started on PORT ${PORT}`);
 });
